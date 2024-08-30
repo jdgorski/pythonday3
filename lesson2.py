9013902197f582d144eaa340c9358793ad7ff811
Gorski <jdgorski@cps.k12.il.us>
2024-08-30 14:51:40 -0500

    first commit

---
+++ lesson2.py
@@ -0,0 +1,62 @@
+# # print("Hello, World!")
+# # day=input("What is the day of the week? ")
+# # print(f'the day of the week is {day}.')
+# movies_this_week = input("What movies are you watching this week? ")
+# print(f'I am watching {movies_this_week} this week.')
+# mood = input("How are you feeling today? ")
+# print(f'I am feeling {mood} today.')
+
+#data types for variables in python
+
+
+# # STRING --> text
+# name = "John"
+# # whenever it is wrapped in quotes it is a string
+
+
+# # INTEGERS --> whole numbers
+
+# year = 2024
+# # DOES NOT NEED to be wrapped in quotes
+# yearFourFromNow = 2028
+# subtract = yearFourFromNow - year
+# print(subtract)
+
+# # FLOATS --> decimal numbers
+
+
+# priceBigMac = 3.99
+# #NEEDS TO HAVE A DECIMAL
+# priceDoublePounder = 4.99
+# totalprice = priceBigMac + priceDoublePounder
+# print(totalprice)
+
+
+
+
+# # BOOLEANS --> True or False
+# isRaining = False
+# print(isRaining)
+
+
+# # LISTS --> a collection of items
+# groceries = ["apples", "bananas", "carrots"]
+# print(groceries)
+
+address = input("What is the address of the restaurant? ")
+dinner = input("What time is the dinner? ")
+pricedinner = input("What is the price of the dinner? ")
+movies = input("What movie are you watching: ")
+pricemovie = input("What is the price of the movie? ")
+moviecheck = input("Was the movie available? (Was/Was not) ")
+movieppl = input("How many people are going to the movies? ")
+timemovie = input("At what time? ")
+print(f"The address of the restaurant is {address}, the time of the dinner is {dinner}, and it costs {pricedinner}. The movie that I am watching is {movies}, the price of the movie is {pricemovie}, the movie {moviecheck} available, and it was at {timemovie}.")
+
+
+
+
+
+
+
+
